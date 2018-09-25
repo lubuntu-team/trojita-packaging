b6215a74c295d1de88e02148a7ad939f8ae27306
Walter Lapchynski <wxl@ubuntu.com>
2018-09-24 22:42:46 -0700

    remove unnecessary files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(trojita):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="trojita" command="/usr/bin/trojita"
--- debian/trojita.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the trojita package
-#
-0 4	* * *	root	[ -x /usr/bin/trojita_maintenance ] && /usr/bin/trojita_maintenance
